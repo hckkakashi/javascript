f48c09f7d113b0ffbb30664e5c4ea433988dd23a
Pratik Bomzan <121595947+hckkakashi@users.noreply.github.com>
2024-06-14 20:52:22 +0545

    java script variables

---
+++ variables.js
@@ -0,0 +1,32 @@
+// let firstname = 'John';
+// console.log(firstname);
+
+// let course = {
+//     name: 'Courses',
+//     hours: 1
+// };
+// console.log(course.name, course.hours);
+// console.log(course['name']);
+// course['name'] ='javascript 101';
+// console.log(course['name']);
+// let property = 'hours';
+// console.log(course[property]);
+
+//arrys
+
+// let productColors =['blue','green','yellow']
+// productColors[1]=42;
+// console.log(productColors[1]);
+// console.log(typeof productColors);
+// console.log(productColors.length);
+
+//functions
+// function sayHi(name){
+//     console.log('Hi '  + name); ;
+// }
+// sayHi('pratik');
+
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+// console.log(multiply(1,3));
